a7bc0a2d1b587b8d4c661618fb54f5fdb8dc01c8
Przemek Sipta <przemek.sipta@gmail.com>
2024-11-17 11:33:24 +0100

    fix: sending prompts to openai API

--- app/components/MakeAnswerForm/useMakeAnswerForm.ts
+++ app/components/MakeAnswerForm/useMakeAnswerForm.ts
@@ -1,4 +1,4 @@
-import { sendPromptToGPT } from "@/utils";
+import {gptMessagesType, sendPromptToGPT} from "@/utils";
 import { useCallback, useEffect, useReducer, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Actions, FormValues, GPTAnswerState, QuestionsWithAnswer } from "./types";
@@ -10,19 +10,31 @@ function getRandomQuestion<T>(questions: T[]) {
   return questions[randomNumber];
 }
 
-export function prepareGPTPrompt(question: string, answer: string, topic: string) {
-  const order = `Act as world calss teacher and expert in ${topic} Below are two things: a "Question" related to a ${topic} (the sentence after "Question:") and "My answer" (the sentences after "Answer:"). Keep in mind that "My answer" might not always be true. Your task is to determine the correct answer for this question and compare it with "My answer." If "My answer" is correct, display "Correct answer!" Otherwise, display "Incorrect answer" and explain why "My answer" was incorrect.`
-  const questionPrompt = `Question\n${question}`;
-  const answerPrompt = `My answer\n${answer}`;
-  const finalPrompt = `${order}\n\n ${answerPrompt}\n\n${questionPrompt}\n\n`;
-  return finalPrompt;
+export function prepareGPTPrompt(question: string, answer: string, topic: string): gptMessagesType[] {
+  const systemRoleDescription = `Act as world calss teacher and expert in ${topic} Below are two things: a "Question" related to a ${topic} (the sentence after "Question:") and "My answer" (the sentences after "Answer:"). Keep in mind that "My answer" might not always be true. Your task is to determine the correct answer for this question and compare it with "My answer." If "My answer" is correct, display "Correct answer!" Otherwise, display "Incorrect answer" and explain why "My answer" was incorrect.`
+  const systemMessage = `${systemRoleDescription}\nQuestion:\n${question}`;
+  const userMessage = `Answer:\n${answer}`;
+  const gptMessages: gptMessagesType[] = [
+    {role: "system", content: systemMessage},
+    {role: "user", content: userMessage }
+  ]
+
+
+  return gptMessages;
 }
 
-export function prepareGPTPromptWithCorrectAnswer(question: string, myAnswer: string, correctAnswer: string, topic: string) {
-  const order = `As a teacher, check if my answer is correct for a question related to ${topic}. I will provide the correct answer for comparison. If my answer is correct, respond with 'Correct answer.' If it is not correct, respond with 'Not correct answer,' show the correct answer, and explain why my answer was not correct.`
-  const prompt = `${order}\n\nQuestion\n ${question}\n\nMy answer\n${myAnswer}\n\nCorrect answer\n${correctAnswer}\n\n`;
-  console.log("PROMPT: ", prompt);
-  return prompt
+export function prepareGPTPromptWithCorrectAnswer(question: string, myAnswer: string, correctAnswer: string, topic: string): gptMessagesType[] {
+  const systemRoleDescription = `As a teacher, check if my answer is correct for a question related to ${topic}. I will provide the correct answer for comparison. If my answer is correct, respond with 'Correct answer.' If it is not correct, respond with 'Not correct answer,' show the correct answer, and explain why my answer was not correct.`
+  const systemMessage = `${systemRoleDescription}\n\nQuestion\n ${question}Correct answer\n${correctAnswer}\n\n`;
+  const userMessage = `My answer: ${myAnswer}`
+
+  const gptMessages: gptMessagesType[] = [
+    {role: 'system', content: systemMessage},
+    {role: "user", content: userMessage}
+  ]
+
+  console.log("PROMPT: ", gptMessages);
+  return gptMessages
 }
 
 const reducer = (state: GPTAnswerState, action: Actions): GPTAnswerState => {
