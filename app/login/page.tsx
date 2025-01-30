0bcf7008de9d184623e84e08aa2b8a66e41c0361
@przemek.sipta <p.sipta@qarbon.it>
2025-01-30 14:20:45 +0100

    fix: types errors

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -56,7 +56,7 @@ export default function LoginPage() {
                     Login
                 </button>
                 <p className={styles.authLink}>
-                    Don't have an account? <Link href="/register">Register</Link>
+                    Dont have an account? <Link href="/register">Register</Link>
                 </p>
             </form>
         </div>
